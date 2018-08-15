cf8a91bd66fb09850b8cf6e851433125f3e1c72f
santiagopalma <san_tiago96@hotmail.com>
2018-08-15 17:48:15 -0500

    controlador

---
+++ SistemaLineaBlancaSA/src/Controladores/ControlAdministrador.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controladores;
+
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author san_t
+ */
+public class ControlAdministrador implements Controlador{
+
+    @Override
+    public void presentarVista() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ SistemaLineaBlancaSA/src/Controladores/Controlador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controladores;
+
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface Controlador extends ActionListener{
+    
+    public void presentarVista();
+    
+}
